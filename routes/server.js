d138161a56c6232eb86e24fe185fc34c683671ae
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-21 00:00:14 -0400

    add my dependency variables to require them in server.js file, assign a port  for the server to listen on. Additionally, added a more flexible app by using enviornment variable PORT to make it more accessible

---
+++ routes/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4007;
+
+app.listen(PORT, () => {
+    console.log(`App is listening on http://localhost:${PORT}.`)
+})
