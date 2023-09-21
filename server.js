4ffbbdda9a521279d8673f9e58cf3786ecaa6335
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-21 01:09:17 -0400

    add a route to return the notes.html page, the verb used is GET

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.urlencoded ({extended: true}));
 
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => {
+    res.sendFile(`${__dirname}/notes.html`)
+})
+
 app.listen(PORT, () => {
     console.log(`App is listening on http://localhost:${PORT}.`)
 });
