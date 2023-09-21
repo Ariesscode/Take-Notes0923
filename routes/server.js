9c0f0963e24bf96ab6a703f14514aacfd8b3a582
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-21 00:07:17 -0400

    add a middleware to create pre routes for everything in the public folder

--- routes/server.js
+++ routes/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4007;
 
+app.use(express.json());
+app.use(express.urlencoded ({extended: true}));
+
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
     console.log(`App is listening on http://localhost:${PORT}.`)
 })
