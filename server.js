b2a2f35507d0d00e3a8a362d92a3d02a78ee602d
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-21 09:23:57 -0400

    change property name of unique identifier to id

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             text,
-            note_id: uuid,
+            id: uuid,
         };
         apiNotes.push(newNote);
         fs.writeFile('./Develop/db/db.json', JSON.stringify(apiNotes), (err) =>
